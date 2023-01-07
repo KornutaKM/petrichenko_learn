bbf2b61a8af68fe4656364822eeb463f8cc0d895
Kirill Kornuta <onemorekorn@gmail.com>
2023-01-07 10:23:21 +0500

    Циклы задачки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,32 @@
-const obj = {
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+// let film2 = prompt('Один из последних просмотренных фильмов?', '')
+// let scoreOfFilm2 = +prompt('На сколько оцените его?', '')
+
+// personalMovieDB.movies[film2] = scoreOfFilm2
+let a
+let b
+let i = 0
+
+
+// while (i < 2) {
+// 	let a = prompt('Один из последних просмотренных фильмов?', '')
+// 	let b = +prompt('На сколько оцените его?', '')
+// 	personalMovieDB.movies[a] = b
+// 	i++
+// }
+
+for (let i = 0; i < 2; i++) {
+  let a= prompt('Один из последних просмотренных фильмов?', '');
+  let b = +prompt('На сколько оцените его?', '');
+  personalMovieDB.movies[a] = b;
 }
-
-obj['b'] = 'Kirill'
-
-console.log(obj['b']);
+console.log(personalMovieDB)
