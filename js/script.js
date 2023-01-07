bde95ab106fdd770b4cf0d0b8647b5ffb9b22048
Kirill Kornuta <onemorekorn@gmail.com>
2023-01-07 12:36:27 +0500

    Циклы задачки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
@@ -8,25 +9,38 @@ const personalMovieDB = {
   privat: false,
 };
 
-// let film2 = prompt('Один из последних просмотренных фильмов?', '')
-// let scoreOfFilm2 = +prompt('На сколько оцените его?', '')
-
-// personalMovieDB.movies[film2] = scoreOfFilm2
-let a
-let b
-let i = 0
-
-
+// let i = 0;
 // while (i < 2) {
-// 	let a = prompt('Один из последних просмотренных фильмов?', '')
-// 	let b = +prompt('На сколько оцените его?', '')
-// 	personalMovieDB.movies[a] = b
-// 	i++
+//   let a = prompt('Один из последних просмотренных фильмов?', '');
+//   let b = +prompt('На сколько оцените его?', '');
+
+//   if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//     i++;
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
 // }
 
 for (let i = 0; i < 2; i++) {
-  let a= prompt('Один из последних просмотренных фильмов?', '');
+
+	let a = prompt('Один из последних просмотренных фильмов?', '');
   let b = +prompt('На сколько оцените его?', '');
-  personalMovieDB.movies[a] = b;
+
+if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
+	personalMovieDB.movies[a] = b;
+	console.log('done');
+ } else {
+	console.log('error');
+	i--;
+ }
 }
-console.log(personalMovieDB)
+
+personalMovieDB.count < 10
+  ? console.log('Просмотрено довольно мало фильмов')
+  : personalMovieDB.count < 30
+  ? console.log('Вы классический зритель')
+  : console.log('Вы киноман');
+console.log(personalMovieDB);
